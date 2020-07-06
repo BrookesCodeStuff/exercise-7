8e6a9ff233228f86fecf1d92b9fd56ce4081fc9c
Brooke <brookepaglia@gmail.com>
2020-07-06 17:34:19 -0400

    Add functionality and styling

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('It works!');
+const todoField = document.querySelector('#todoField');
+const todoListDisplay = document.querySelector('#todoList');
+let todoList = [];
+
+document.querySelector('#todoForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  todoList.push(todoField.value);
+
+  let template = todoList.map(item => `
+  <li>${item}</li>
+  `)
+
+  todoListDisplay.innerHTML = template.join('');
+  todoField.value = '';
+  todoField.focus();
+});
